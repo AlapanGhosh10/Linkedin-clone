079e10e64ff4fd97ded1b8c823c9d0cdd7e3d0bd
AlapanGhosh10 <alapanzico10@gmail.com>
2023-06-01 21:38:13 +0530

    header complete

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Header.css";
+import HeaderOption from "./HeaderOption";
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
 
 function Header() {
   return (
@@ -15,7 +22,12 @@ function Header() {
       </div>
 
       <div className="header_right">
-
+        <HeaderOption Icon={HomeIcon} title='Home'/>
+        <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+        <HeaderOption Icon={ChatIcon} title='Messaging' />
+        <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+        <HeaderOption avatar="https://cdn-icons-png.flaticon.com/512/149/149071.png" title="me" />
       </div>
     </div>
   );
